027447ba0c5f461a720bd13ebc2c7d5b3b9e46d1
gomyo <cjw1102@naver.com>
2020-03-26 23:17:24 +0900

    study

--- Programmers/최준원_[2]프린터.py
+++ Programmers/최준원_[2]프린터.py
@@ -1,7 +1,9 @@
 def solution(p,l):
     # enumerate를 사용하여 idx 위치를 정해 놓는다
-    for idx, pt in enumerate(p):
-        if 
+    while True:
+        if p = []:
+            break
+        
     maxP = p.index(max(p))
     p = sorted(p[maxP:],reverse=True)+sorted(p[:maxP],reverse=True)
 
