c9f07140aafe724855cc037d7b4bdfe310de7112
Gomyo <cjw1102@naver.com>
2020-03-30 16:01:15 +0900

    study

--- Programmers/최준원_[2]H-Index.py
+++ Programmers/최준원_[2]H-Index.py
@@ -13,5 +13,5 @@ def solution(citations):
     for i, j in enumerate(citations):
         if i >= j:
             return i
-    # [10, 50, 100]처럼 최하 value도 
+    # [10, 50, 100]처럼 최하 value가 어떤 idx보다 클 경우 리스트의 길이 자체를 반환해야 함
     return lc
