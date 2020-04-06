e025c6f909f00c861977efbbaaf65a38fdf2d60d
Gomyo <cjw1102@naver.com>
2020-04-06 20:18:03 +0900

    study

---
+++ Programmers/최준원/[2]카펫.py
@@ -0,0 +1,7 @@
+def solution(b,r):
+    # x축 y축을 생각하면 전체 크기가 x*y이고 red 크기는 (x-2)(y-2)임을 알 수 있다.
+    # 계산해보면 2x^2 - (brown+4)x + 2red + 2brown = 0
+    # x = -b + sqrt(b^2-4ac)/2a (근의 공식)으로 계산
+    x = ((b+4) + ((b+4)**2 - 16*(b+r))**0.5)/4
+    y = (b+r)//x
+    return [max(x,y),min(x,y)]
