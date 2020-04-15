7fce156430c966cb670a05761e2e553f0aee4e95
Gomyo <cjw1102@naver.com>
2020-04-15 20:17:40 +0900

    simplify

--- Programmers/최준원/[2]다음 큰 숫자.py
+++ Programmers/최준원/[2]다음 큰 숫자.py
@@ -1,4 +1,3 @@
-# 이게 왜 안되는거지?
 def solution(n):
     ans = 0
     # bin 함수를 사용하면 str 형으로 반환된다는 것을 알았음.
@@ -12,12 +11,9 @@ def solution(n):
     return ans
 
 # 가장 간단하게 풀어봄
-
 def solution(n):
     one = bin(n).count('1')
     for i in range(n+1,1000001):
         if bin(i).count('1') == one:
             return i
-
-
 print(solution(78))
