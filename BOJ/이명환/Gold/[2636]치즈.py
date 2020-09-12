92e62c121fffdcd1fb84247f9fc7a080f9068a6f
Gobans <audghks4452@gmail.com>
2020-09-12 23:51:38 +0900

    2020.09.12

---
+++ BOJ/이명환/Gold/[2636]치즈.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+m,n = map(int,sys.stdin.readline().split())
+cheez = [list(map(int,input().split())) for _ in range(m)]
+
+#치즈가 다 사라졌는지 확인
+xdir = [0,1,-1,0]
+ydir = [-1,0,0,1]
+
+day = 0
+remain = 0
+while True:
+    before_remain = remain
+    remain =0
+    visited = [[False] * n for i in range(m)]
+    q = deque((0,0))
+    while q:
+        row,column = q.popleft()
+        if cheez[row][column] != 0:
+            remain +=1
+        elif cheez[row][column] == 0 and visited[row][column] != True:
+            for i in range(4):
+                x = xdir[i] + column
+                y = ydir[i] + row
+                if 0<=x<=n-1 and 0<=y<=m-1:
+                    if cheez[y][x] !=0 and cheez[y][x] !=0:
+                        cheez[y][x] = 0
+                        visited[y][x] = True
+    day +=1
+    if remain == 0:
+        print(day)
+        print(before_remain)
+        break
+
+    # 함수로 구현하는게 나을듯? 큐로 하니까 날짜 구분이 어려운 것 같다.
+
--- BOJ/이명환/Gold/[9466]텀 프로젝트.py
+++ BOJ/이명환/Gold/[9466]텀 프로젝트.py
@@ -1,42 +1,70 @@
+# import sys
+# from collections import deque
+#
+# t = int(sys.stdin.readline())
+# for i in range(t):
+#     group = 0
+#     ans = 0
+#     n = int(sys.stdin.readline())
+#     student = list(map(int,sys.stdin.readline().split()))
+#     student.insert(0,0)
+#
+#     visit = [0 for _ in range (n+1)]
+#     student_deque = deque(student)
+#     idx = n
+#     first_node = False
+#     while student_deque:
+#         node = student_deque.pop()
+#         if first_node == node:
+#             ans += group
+#             first_node = False
+#             idx = len(student_deque) -1
+#             group = 0
+#             continue
+#         if first_node == False:
+#             first_node = idx
+#         if visit[node] == 1:
+#             first_node = False
+#             idx = len(student_deque) -1
+#             group = 0
+#             continue
+#         if node == idx:
+#             ans +=1
+#             first_node = False
+#             idx = len(student_deque) -1
+#             continue
+#         group +=1
+#         visit[node] = 1
+#         student_deque.append(student[node])
+#         idx = node
+#     print(n-ans)
+
+#대폭개선 필요할듯 설계를 좀 잘못한 느낌쓰..
 import sys
-from collections import deque
+sys.setrecursionlimit(999999)
+
+def DFS(x):
+    global result
+    visited[x] = True
+    cycle.append(x)
+    number = students[x]
+
+    if visited[number]:
+        if number in cycle:
+            result += cycle[cycle.index(number):]
+        return
+    else:
+        DFS(number)
 
 t = int(sys.stdin.readline())
 for i in range(t):
-    group = 0
-    ans = 0
     n = int(sys.stdin.readline())
-    student = list(map(int,sys.stdin.readline().split()))
-    student.insert(0,0)
+    students = [0] + list(map(int,sys.stdin.readline().split()))
+    visited = [True] + [False] *n
+    result = []
 
-    visit = [0 for _ in range (n+1)]
-    student_deque = deque(student)
-    idx = n
-    first_node = False
-    while student_deque:
-        node = student_deque.pop()
-        if first_node == node:
-            ans += group
-            first_node = False
-            idx = len(student_deque) -1
-            group = 0
-            continue
-        if first_node == False:
-            first_node = idx
-        if visit[node] == 1:
-            first_node = False
-            idx = len(student_deque) -1
-            group = 0
-            continue
-        if node == idx:
-            ans +=1
-            first_node = False
-            idx = len(student_deque) -1
-            continue
-        group +=1
-        visit[node] = 1
-        student_deque.append(student[node])
-        idx = node
-    print(n-ans)
-
-#대폭개선 필요할듯 설계를 좀 잘못한 느낌쓰.. 
+    for i in range(1,n+1):
+        if not visited[i]:
+            cycle = []
+            DFS(i)
+    print(n - len(result))
