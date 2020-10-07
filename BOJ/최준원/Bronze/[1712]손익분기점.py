a37512a1047a7dbbe9a28362b32fac167de5fcd7
Joonwon Choi <engcraft1102@gmail.com>
2020-10-08 00:34:58 +0900

    Math

--- BOJ/최준원/Bronze/[1712]손익분기점.py
+++ BOJ/최준원/Bronze/[1712]손익분기점.py
@@ -1,6 +1,6 @@
-a, b, c = map(int, input().split())
 '''
 시간 초과
+a, b, c = map(int, input().split())
 if b>=c:
     print(-1)
 res = 1
@@ -12,3 +12,9 @@ while a+b >= c:
 print(res)
 '''
 # 21억 이하의 자연수가 주어지고, 0.35초안에 풀어야 하기 때문에 공식을 찾아야 할 듯.
+# a < cx - bx
+a, b, c = map(int, input().split())
+if b >= c:
+    print(-1)
+else:
+    print(a//(c-b)+1)
