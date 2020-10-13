9ca1abfe11cf6efc5da9ba763134c55d90cb884a
Joonwon Choi <engcraft1102@gmail.com>
2020-10-14 02:32:48 +0900

    수학

--- BOJ/최준원/Bronze/[1193]분수찾기.py
+++ BOJ/최준원/Bronze/[1193]분수찾기.py
@@ -1,12 +1,10 @@
 # 0.5s Time limit
-n, x, y = int(input()), 0, 0
-while n>0:
-    n-=x
-    x+=1
-    if n<=x:
-        y=n
-        break
-son = x
-
-if x%2==0:
-    print(x+'/'+y)
+x, row, end = int(input()), 0, 0
+while x>end:
+    row+=1
+    end+=row
+a = end - x
+if row%2 == 0:
+    print(f'{row-a}/{a+1}')
+else:
+    print(f'{a+1}/{row-a}')
