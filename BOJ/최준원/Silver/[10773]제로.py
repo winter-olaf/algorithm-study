9c712ba99d437c7bc44fc98e3ba65a64379e3c06
Joonwon Choi <engcraft1102@gmail.com>
2020-10-21 00:39:05 +0900

    스택

--- BOJ/최준원/Silver/[10773]제로.py
+++ BOJ/최준원/Silver/[10773]제로.py
@@ -1,6 +1,19 @@
+# 4008ms
+'''
 stack = []
 for _ in range(int(input())):
     e = int(input())
+    if e == 0:
+        stack.pop()
+    else:
+        stack.append(e)
+print(sum(stack))
+'''
+# 108ms
+from sys import stdin
+stack = []
+for _ in range(int(input())):
+    e = int(stdin.readline())
     if e == 0:
         stack.pop()
     else:
