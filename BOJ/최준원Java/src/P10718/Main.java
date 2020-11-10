861859bce24827c958dd7e4e849497ccb8a398ce
Joonwon <engcraft1102@gmail.com>
2020-11-10 21:42:22 +0900

    java

--- BOJ/최준원Java/src/P10718/Main.java
+++ BOJ/최준원Java/src/P10718/Main.java
@@ -1,2 +1,8 @@
-package P10718;public class Main {
+package P10718;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
 }
--- BOJ/최준원Java/src/P2557/Main.java
+++ BOJ/최준원Java/src/P2557/Main.java
@@ -1,7 +1,7 @@
-package 2557;
+package P2557;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
     }
 }
