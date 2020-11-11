9b248153ec22f4da2015567a2ae1a44afb2ae051
Gobans <audghks4452@gmail.com>
2020-11-11 20:55:26 +0900

    2020.11.11

--- BOJ/이명환/Silver/C++/[2644]촌수계산.cpp
+++ BOJ/이명환/Silver/C++/[2644]촌수계산.cpp
@@ -1,36 +1,86 @@
-//2644 - recursion
-#include <queue>
-#include <iostream>
-#include <vector>
+//#include <queue>
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector < vector <int> > family(101);
+//bool chk[101];
+//int x, y;
+//vector <int> count_list(101);
+//
+//int dfs(int node, int count) {
+//    chk[node] = true;
+//    if (node == y) return count;
+//    for (int i = 0; i < family[node].size(); i++) {
+//        if (chk[family[node][i]] == false) {
+//            return dfs(family[node][i], count + 1);
+//        }
+//    }
+//    return 0;
+//}
+//
+//int main() {
+//    int N; cin >> N;
+//    cin >> x >> y;
+//    int V; cin >> V;
+//
+//
+//    for (int i = 0; i < V; i++) {
+//        int f, l; cin >> f >> l;
+//        family[f].push_back(l);
+//        family[l].push_back(f);
+//    }
+//
+//    int ans = dfs(x,0);
+//   
+//
+//    if (ans == 0) cout << "-1" << endl;
+//    else cout << ans << endl;
+//
+//}
 
+#include<iostream>
+#include<queue>
 using namespace std;
 
-vector <vector <int> > fam(101);
-int chk[101];
-int start, end1;
+queue<int> q;
 
-int dfs(int person, int count){
-    chk[person] = 1;
-    if(person == end1) return count;
-    for(int i = 0; i < fam[person].size(); i++){
-        if (chk[fam[person][i]] == false){
-            return dfs(fam[person][i], count+1);
+int N, M;
+int desx, desy;
+int map[101][101];
+int visited[101];
+
+void BFS() {
+    int i;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for (i = 1; i <= N; i++) {
+            if (map[cur][i] == 1 && visited[i] == 0) {
+                visited[i] = visited[cur] + 1;
+                q.push(i);
+            }
         }
     }
-    return 0;
 }
 
-int main(){
-    int N; cin >> N;
-    cin >> start >> end1;
-    int m; cin >> m;
-    for(int i = 0; i < m; i++){
-        int x, y; cin >> x >> y;
-        fam[x].push_back(y); fam[y].push_back(x);
+int main()
+{
+    cin >> N;
+    cin >> desx >> desy;
+    cin >> M;
+
+    int i, j;
+    int x, y;
+    for (i = 0; i < M; i++) {
+        cin >> x >> y;
+        map[x][y] = map[y][x] = 1;
     }
-    
-    int ans = dfs(start, 0);
-    
-    if(ans== 0) cout << "-1\n";
-    else cout << ans << '\n';
+
+    visited[desx] = 1;
+    q.push(desx);
+    BFS();
+    cout << visited[desy] - 1;
+    return 0;
 }
