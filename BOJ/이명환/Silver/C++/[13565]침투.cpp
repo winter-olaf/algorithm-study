4ad31880325cd9bbc942795759596067b5f74ce6
Gobans <audghks4452@gmail.com>
2020-11-13 23:18:14 +0900

    2020.11.13

--- BOJ/이명환/Silver/C++/[13565]침투.cpp
+++ BOJ/이명환/Silver/C++/[13565]침투.cpp
@@ -1,33 +1,47 @@
-#include <queue>
 #include <iostream>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
 int mat[1001][1001];
 int chk[1001][1001];
-int M,N;
-int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+int M, N;
+int dir[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
 
-void dfs(int y,int x){
-    chk[y][x] =1;
-    for(int i =0; i);
+
+void dfs(int y, int x) {
+    chk[y][x] = 1;
+    for (int i = 0; i < 4; i++) {
+        int yy = y + dir[i][0], xx = x + dir[i][1];
+        if (yy < 0 || yy >= M || xx < 0 || x >= N) continue;
+        if (mat[yy][xx] == 0 && chk[yy][xx] == 0) {
+            dfs(yy, xx);
+        }
+    }
+
 }
 
-int main(){
-    cin>> M >>N;
-    for (int i = 0; i< M; i++){
-        for(int j =0; j> N; j++){
-            scanf("%1d",&mat[i][j]);
+
+int main()
+{
+    cin >> M >> N;
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf_s("%1d", &mat[i][j]);
         }
-    }
-    for(int i=0; i <N; i++){
-        if(mat[0][i] ==0){
-            dfs(0,i);
-        }
-    }
-    for(int i=0; i <N; i++){
-        if(mat[])
     }
 
+    for (int i = 0; i < N; i++) {
+        if (mat[0][i] == 0) {
+            dfs(0, i);
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (chk[M-1][i] == 1) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
 }
