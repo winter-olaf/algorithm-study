e103b7038f39a0d704223fbcd95c6241cf0bcdc2
Joonwon Choi <engcraft1102@gmail.com>
2020-11-14 20:07:08 +0900

    문자열

--- BOJ/최준원 Java/src/문자열/P5622/Main.java
+++ BOJ/최준원 Java/src/문자열/P5622/Main.java
@@ -13,24 +13,25 @@ public class Main {
         }
         System.out.println(res);
     }
-    static int dial(char c) {
+    public static int dial(char c) {
         switch (c) {
-            case "A": case "B": case "C":
+            case 'A': case 'B': case 'C':
                 return 3;
-            case "D": case "E": case "F":
+            case 'D': case 'E': case 'F':
                 return 4;
-            case "G": case "H": case "I":
+            case 'G': case 'H': case 'I':
                 return 5;
-            case "J": case "K": case "L":
+            case 'J': case 'K': case 'L':
                 return 6;
-            case "M": case "N": case "O":
+            case 'M': case 'N': case 'O':
                 return 7;
-            case "P": case "Q": case "R": case "S":
+            case 'P': case 'Q': case 'R': case 'S':
                 return 8;
-            case "T": case "U": case "V":
+            case 'T': case 'U': case 'V':
                 return 9;
-            case "W": case "X": case "Y": case "Z":
+            case 'W': case 'X': case 'Y': case 'Z':
                 return 10;
         }
+        return 0;
     }
 }
