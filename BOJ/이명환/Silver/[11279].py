7bcffbe2ad4046c07e28664ca7ecc989bf2bc590
Gobans <audghks4452@gmail.com>
2020-12-06 10:16:34 +0900

    2020.12.06 heap

---
+++ BOJ/이명환/Silver/[11279].py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if not heap:
+            print('0')
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (-x,x))
