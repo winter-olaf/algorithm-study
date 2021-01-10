42494ed6937ea53a549c83062d70e3a056186b45
Gobans <audghks4452@gmail.com>
2021-01-10 11:36:40 +0900

    2021.01.10 dp

--- BOJ/이명환/Silver/[14501]퇴사.py
+++ BOJ/이명환/Silver/[14501]퇴사.py
@@ -1,8 +1,16 @@
 import sys
 n = int(sys.stdin.readline())
-t_p = []
+t,p = [0 for i in range(n+1)],[0 for i in range(n+1)]
 for i in range(n):
-    t,p = map(int,sys.stdin.readline().split())
-    t_p.append((t,p))
+    a,b = map(int,sys.stdin.readline().split())
+    t[i] = a
+    p[i] = b
 
-print(t_p)
+dp = [0 for i in range(n+1)]
+
+for i in range(len(t)-2,-1,-1):
+    if t[i]+i <= n:
+        dp[i] = max(p[i]+dp[i+t[i]], dp[i+1])
+    else:
+        dp[i]=dp[i+1]
+print(dp[0])
